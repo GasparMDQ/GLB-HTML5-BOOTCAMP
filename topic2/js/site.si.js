dbe0c29fb8ce904aaafbeca06f976190e45e78a2
Gaspar Zaragoza <gaspar.zaragoza@globant.com>
2013-11-14 14:46:14 -0300

    Topic 2
    Newspaper with foundation

--- topic2/js/site.si.js
+++ topic2/js/site.si.js
@@ -1,3 +1,10 @@
 $(function() {
+  $('dl.sub-nav > dd').click(function(){
+    $(this).parent().children().removeClass('active');
+    $(this).addClass('active');
+  });
+  if ( $('body').attr('data-css') == 'foundation') {
+    $(document).foundation();
+  }
 });
 
