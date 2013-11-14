0fa5d0c4acf1c8cdb6515a06834e5b475767a678
Gaspar Zaragoza <gaspar.zaragoza@globant.com>
2013-11-14 16:10:20 -0300

    Topic 2
    Newspaper with Bootstrap

--- topic2/js/site.si.js
+++ topic2/js/site.si.js
@@ -1,10 +1,13 @@
 $(function() {
-  $('dl.sub-nav > dd').click(function(){
+  $('dl.sub-nav > dd, .nav-tabs > li').click(function(){
     $(this).parent().children().removeClass('active');
     $(this).addClass('active');
   });
   if ( $('body').attr('data-css') == 'foundation') {
     $(document).foundation();
+  }
+  if ( $('body').attr('data-css') == 'bootstrap') {
+    $('.carousel').carousel();
   }
 });
 
